130686dc9d2b7b06993aa214be911b9be1906187
arielito <ariel@bewise.com.es>
2024-08-27 18:14:37 -0300

    TP triangulos, principios de calidad de software

---
+++ Programacion Inicial/Ejercicios/Unidad 5/Parte 1/ejercicio21/cmake-build-debug/CMakeFiles/ejercicio21.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "F:/CLion Workspace/Universidad Nacional de la Matanza/Universidad-Nacional-de-La-Matanza/Programacion Inicial/Ejercicios/Unidad 5/Parte 1/ejercicio21/main.c" "CMakeFiles/ejercicio21.dir/main.c.obj" "gcc" "CMakeFiles/ejercicio21.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Programacion Inicial/Ejercicios/Unidad 5/Parte 1/ejercicio21/cmake-build-debug/CMakeFiles/ejercicio21.dir/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/ejercicio21.dir/main.c.obj: \
+ F:\CLion\ Workspace\Universidad\ Nacional\ de\ la\ Matanza\Universidad-Nacional-de-La-Matanza\Programacion\ Inicial\Ejercicios\Unidad\ 5\Parte\ 1\ejercicio21\main.c \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ F:/Archivos\ de\ Programa/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
